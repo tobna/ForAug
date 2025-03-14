5e583b72d3af0d8006f459fdb0c58f68ec1d2d73
Tobias Christian Nauen <tobias_christian.nauen@dfki.de>
2025-03-14 11:16:58 +0100

    more workers for full train set Diff calculation

--- sbatch_diffs_train.sh
+++ sbatch_diffs_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --array=0-299%100
+#SBATCH --array=0-399
 #SBATCH --time=1-0
 #SBATCH --mem=2G
 #SBATCH --partition=H200,H100,H100-PCI,A100-PCI,A100-40GB,A100-80GB
@@ -16,4 +16,4 @@ srun -K \
   --container-image=/netscratch/nauen/images/create_in_diff.sqsh \
   --container-workdir="`pwd`" \
   --container-mounts=/netscratch/$USER:/netscratch/$USER,/fscratch/$USER:/fscratch/$USER,/ds-sds:/ds-sds:ro,/ds:/ds:ro,"`pwd`":"`pwd`" \
-  python3 make_diffs.py -n 300 -id $SLURM_ARRAY_TASK_ID -w 16 -in /ds-sds/images/imagenet -fn /fscratch/nauen/datasets/INSegment_v1_f1 -s train -o /netscratch/nauen/datasets/INDiffs
+  python3 make_diffs.py -n 400 -id $SLURM_ARRAY_TASK_ID -w 16 -in /ds-sds/images/imagenet -fn /fscratch/nauen/datasets/INSegment_v1_f1 -s train -o /netscratch/nauen/datasets/INDiffs
