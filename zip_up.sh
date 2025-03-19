27eb8bf88239a1d04e2904de59a1b46aa27f5f8c
Tobias Christian Nauen <tobias_christian.nauen@dfki.de>
2025-03-19 13:01:07 +0100

    script to zip up the files

---
+++ zip_up.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+cd $1
+
+echo "zip train/foregrounds"
+zip -r0 foregrounds_train.zip train/foregrounds | pv -lep -s 1275557 > /dev/null
+
+echo "zip train/backgrounds"
+zip -r0 backgrounds_train.zip train/backgrounds | pv -lep -s 1275556 > /dev/null
+
+echo "zip val/foregrounds"
+zip -r0 foregrounds_val.zip val/foregrounds | pv -lep -s 50751 > /dev/null
+
+echo "zip val/backgrounds"
+zip -r0 backgrounds_val.zip val/backgrounds | pv -lep -s 50751 > /dev/null
+
