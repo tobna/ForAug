46a13d3129b58796a495eb9a143379e63afac29a
Tobias Christian Nauen <tobias_christian.nauen@dfki.de>
2025-03-19 16:03:08 +0100

    also download the fg_bg_ratios

--- download_diff_files.sh
+++ download_diff_files.sh
@@ -3,6 +3,6 @@
 path="`pwd`"
 cd $1
 
-wget2 "https://huggingface.co/datasets/TNauen/ForNet/resolve/main/"{train_0.zip,train_1.zip,train_2.zip,train_3.zip,train_4.zip,train_5.zip,train_6.zip,train_7.zip,train_8.zip,train_9.zip,train_10.zip,train_11.zip,train_12.zip,train_13.zip,train_14.zip,train_15.zip,train_16.zip,train_17.zip,train_18.zip,train_19.zip,val.zip}
+wget2 "https://huggingface.co/datasets/TNauen/ForNet/resolve/main/"{train_0.zip,train_1.zip,train_2.zip,train_3.zip,train_4.zip,train_5.zip,train_6.zip,train_7.zip,train_8.zip,train_9.zip,train_10.zip,train_11.zip,train_12.zip,train_13.zip,train_14.zip,train_15.zip,train_16.zip,train_17.zip,train_18.zip,train_19.zip,val.zip,fg_bg_ratios_train.json,fg_bg_ratios_val.json}
 
 cd $path
